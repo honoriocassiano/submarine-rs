a5b65469aa2c9e13ed88053fe356d42e14ade9b9
Cassiano Honorio da Silva <honoriocassiano@gmail.com>
2021-09-02 04:45:36 -0300

    Refactor resource management

--- src/resources.rs
+++ src/resources.rs
@@ -1,32 +1,35 @@
+use std::collections::HashMap;
 use std::rc::Rc;
 
+use sdl2::image::LoadSurface;
 use sdl2::mixer::Music;
 use sdl2::surface::Surface;
 use sdl2::ttf::Font;
 
-enum ResourceType {
-    Audio(&'static str),
-    Font(&'static str),
-    Image(&'static str),
-    Music(&'static str),
+pub struct ResourceManager<'a> {
+    resources: HashMap<&'static str, Surface<'a>>,
 }
 
-trait Resource {}
-
-impl Resource for Surface<'_> {}
-
-impl Resource for Music<'_> {}
-
-impl Resource for Font<'_, '_> {}
-
-struct ResourceManager {}
-
-impl ResourceManager {
-    pub fn new() -> Result<ResourceManager, String> {
-        Ok(ResourceManager {})
+impl<'a> ResourceManager<'a> {
+    pub fn new() -> Result<Self, String> {
+        Ok(Self {
+            resources: HashMap::default(),
+        })
     }
 
-    pub fn load(resource: ResourceType) -> Rc<dyn Resource> {
+    pub fn load_image(&mut self, filename: &'static str) -> &Surface<'a> {
+        let surface = Surface::from_file(filename).unwrap();
+
+        self.resources.insert(filename, surface).unwrap();
+
+        self.resources.get(filename).unwrap()
+    }
+
+    pub fn load_audio(&mut self, _filename: &'static str) -> Rc<Music<'a>> {
+        todo!()
+    }
+
+    pub fn load_font<'b>(&mut self, _filename: &'static str) -> Rc<Font<'a, 'b>> {
         todo!()
     }
 }
